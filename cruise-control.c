a2fad5a98369e51251400b5c6ac91fe313a3d31c
Carlos Pasquali <carlosdpasqualip@hotmail.com>
2023-06-12 21:50:38 -0300

    master - fapplyLowPassFilter

--- cruise-control.c
+++ cruise-control.c
@@ -1,49 +1,75 @@
 // Variáveis globais
-float v = 0;  // velocidade atual
+float velocity = 0;  // velocidade atual
+float filteredVelocity = 0;  // velocidade filtrada
 float u[] = {100, 110, 100, 90};  // vetor de valores de entrada u[n]
 int uIndex = 0;  // índice atual do vetor de valores de entrada u[n]
-unsigned long previousTimeVelocity = 0;  // variável para controle do tempo para velocidade
-unsigned long previousTimeSample = 0;  // variável para controle do tempo para coleta da amostra
-unsigned long interval = 50;  // intervalo de tempo entre as atualizações, em milissegundos
+
+// Parâmetros do filtro passa-baixa
+float k = 0.2;  // Ganho do filtro
+float w0 = 1.0;  // Frequência de corte do filtro
+
+// Variaveis para controlar os intervalos
+unsigned long previousTimeVelocity = 0;  // velocidade
+unsigned long previousTimeSample = 0;  // tempo de coleta
+
+// Intervalos
+unsigned long sampleInterval = 50;  // intervalo de tempo entre as atualizações, em milissegundos
+unsigned long velocityInterval = 15000;  // intervalo de tempo entre as atualizações, em milissegundos
 
 // Função para gerar ruído randômico com amplitude de 0.05
 float generateRandomNoise() {
-  return (random(-100, 101) / 100.0) * 0.05;
+  return (random(-100, 101) / 100.0) * 0.05; // retorna um valor entre 0 e 1
+}
+
+void applyLowPassFilter() {
+  // Coeficientes do filtro passa-baixa
+  float alpha = 0.05;  // Fator de suavização quanto menor melhor
+  //Esse fator influência na qualidade 
+  //e na rapidez de adapatação do sinal durante a filtragem
+
+  // O alpha serve para pegar uma porcentagem do valor atual e uma 
+  // porcentagem do valor anterior. Quanto menor a contribuição da velocidade 
+  // atual no valor filtrado menos frequencias altas passarão, e vice-versa.
+  // O complemento de Alpha (1- alpha) serve para reutilizar o valor anterior
+  // na média calculada.
+  filteredVelocity = alpha * velocity + (1 - alpha) * filteredVelocity;
+}
+
+void getVelocity(unsigned long currentTime) {
+  if (currentTime - previousTimeVelocity >= velocityInterval) {
+    previousTimeVelocity = currentTime;
+
+    // Utiliza o operador % para pegar o resto da divisão
+    // do index com o tamanho máximo do array utilizando a função sizeof
+    // dividido por uma posição qualquer em bytes
+    uIndex = (uIndex + 1) % (sizeof(u) / sizeof(u[0]));
+  }
+}
+
+void updateVelocity(unsigned long currentTime) {
+  if (currentTime - previousTimeSample >= sampleInterval) {
+    // uValue recebe a velocidade de referência
+    float uValue = u[uIndex];
+
+    //float previousVelocity = velocity;
+
+    // Simulação da dinâmica de aceleração do veículo
+    velocity = 0.9753 * velocity + 0.02469 * uValue + generateRandomNoise();
+
+    // Aplica o filtro passa-baixa
+    applyLowPassFilter();
+
+    previousTimeSample = currentTime;  // atualiza o tempo da última atualização
+
+    Serial.print(velocity);
+    Serial.print(",");
+    Serial.println(filteredVelocity);
+  }
 }
 
 void setup() {
   // Inicialização do Arduino
   Serial.begin(9600);
-}
-
-void getVelocity(unsigned long currentTime){
-
-  if (currentTime - previousTimeVelocity >= 15000) {
-    previousTimeVelocity = currentTime;
-
-    // Atualiza o valor de entrada u[n]
-   
-    uIndex = (uIndex + 1) % (sizeof(u) / sizeof(u[0]));
-
-    Serial.println(uIndex);
-  }
-
-}
-
-void updateVelocity(unsigned long currentTime){
-
-   if (currentTime - previousTimeSample >= interval) {
-    // Atualiza o valor de entrada u[n]
-    float uValue = u[uIndex];
-
-    // Simulação da dinâmica do veículo
-    v = 0.9753 * v + 0.02469 * uValue + generateRandomNoise();
-
-    // Imprime a velocidade atual
-
-    previousTimeSample = currentTime;  // atualiza o tempo da última atualização
-  Serial.println(v);
-  }
 }
 
 void loop() {
@@ -52,18 +78,7 @@ void loop() {
   // Verifica se passaram 15 segundos desde a última atualização
   getVelocity(currentTime);
 
-
   // Verifica se é hora de atualizar a velocidade
   updateVelocity(currentTime);
-  // if (currentTime - previousTimeSample >= interval) {
-  //   // Atualiza o valor de entrada u[n]
-  //   float uValue = u[uIndex];
 
-  //   // Simulação da dinâmica do veículo
-  //   v = 0.9753 * v + 0.02469 * uValue + generateRandomNoise();
-
-  //   // Imprime a velocidade atual
-
-  //   previousTimeSample = currentTime;  // atualiza o tempo da última atualização
-  // }
 }
