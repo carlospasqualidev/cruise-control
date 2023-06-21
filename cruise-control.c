c410a4d12f1dbd383a056dcc8803a38ef8b2699e
Carlos Pasquali <carlosdpasqualip@hotmail.com>
2023-06-21 15:57:43 -0300

    master - finish project

--- cruise-control.c
+++ cruise-control.c
@@ -3,10 +3,18 @@ float velocity = 0;  // velocidade atual
 float filteredVelocity = 0;  // velocidade filtrada
 float u[] = {100, 110, 100, 90};  // vetor de valores de entrada u[n]
 int uIndex = 0;  // índice atual do vetor de valores de entrada u[n]
+float d[] = {10,-20,55,-30};
 
-// Parâmetros do filtro passa-baixa
-float k = 0.2;  // Ganho do filtro
-float w0 = 1.0;  // Frequência de corte do filtro
+float ki = 1.8;
+float kp = 5.4;
+float i = 0;
+float p = 0;
+float KiTime = 0.05;
+
+float LastI = 0;
+float error = 0;
+float referenceValue = 0;
+
 
 // Variaveis para controlar os intervalos
 unsigned long previousTimeVelocity = 0;  // velocidade
@@ -51,15 +59,36 @@ void updateVelocity(unsigned long currentTime) {
     // uValue recebe a velocidade de referência
     float uValue = u[uIndex];
 
-    //float previousVelocity = velocity;
+    //Item 3.b disturbio aplicado
+    // float uValue = u[0];
+    // float dValue = d[uIndex];
 
+    //Itens 1 e 2
     // Simulação da dinâmica de aceleração do veículo
     velocity = 0.9753 * velocity + 0.02469 * uValue + generateRandomNoise();
+
+    //Item 3
+    //velocity = 0.9753 * velocity + 0.02469 * referenceValue + generateRandomNoise();
+    
+    //Item 3.b disturbio aplicado na saida
+    // velocity = 0.9753 * velocity + 0.02469 * referenceValue + generateRandomNoise() +dValue;
 
     // Aplica o filtro passa-baixa
     applyLowPassFilter();
 
     previousTimeSample = currentTime;  // atualiza o tempo da última atualização
+
+    error = uValue - filteredVelocity;
+
+    p = kp * error;
+    i = LastI + error * ki * KiTime;
+    referenceValue = p + i;
+
+    //item 3.b disturbio aplicado na entrada
+    //referenceValue = p + i + dValue;
+    
+    LastI = i;
+
 
     Serial.print(velocity);
     Serial.print(",");
